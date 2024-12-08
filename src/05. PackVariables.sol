308523e6905ce6362c92874a041af12a82e39830
Vyacheslav Ryzhkov <rugrisser@gmail.com>
2024-12-08 18:58:05 +0300

    Optimize 05

--- src/05. PackVariables.sol
+++ src/05. PackVariables.sol
@@ -24,9 +24,25 @@ contract PackVariables is IPackVariables {
 }
 
 contract PackVariablesOptimized is IPackVariables {
-    /* YOUR SOLUTION GOES HERE */
+
+    uint256 firstSlot;
+    uint256 secondSlot;
+    bytes32 thirdSlot;
 
     function setValues(uint8 _one, uint256 _two, bytes18 _three, uint8[30] calldata _four, bytes14 _five, uint8 _six)
-        public
-    {}
+        external
+    {
+        unchecked {
+            uint256 result = (uint256(_one) << 248) + (uint256(_six) << 240);
+
+            for (uint8 i = 0; i < 30; ++i) {
+                result += uint256(_four[i]) << (i * 8);
+            }
+
+            firstSlot = result;
+        }
+
+        secondSlot = _two;
+        thirdSlot = (bytes32(_five) >> 144) | _three;
+    }
 }
